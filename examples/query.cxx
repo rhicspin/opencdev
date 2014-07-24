d035b5ce994afded9fce4bdb44a56d77619e4c7b
Dmitry Kalinkin <dkalinkin@bnl.gov>
2014-07-24 12:33:11 -0400

    output values in examples/query tool

--- examples/query.cxx
+++ examples/query.cxx
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 #include <opencdev.h>
 
@@ -16,11 +17,29 @@ int main(void)
    opencdev::result_t result;
    db.query_fill("RHIC/Polarimeter/Blue/biasReadbacks.logreq", 17104, &result);
 
+   cout << std::fixed << std::setprecision(1);
+
+   cout << "Time\t";
    for(opencdev::result_t::const_iterator it = result.begin(); it != result.end(); it++)
    {
       const string &cell_name = it->first;
       const map<opencdev::cdev_time_t, double> &values = it->second;
-      cout << cell_name << endl;
+      cout << cell_name << "\t";
+   }
+   cout << endl;
+
+   const map<opencdev::cdev_time_t, double> &time_map = result.begin()->second;
+   for(map<opencdev::cdev_time_t, double>::const_iterator tit = time_map.begin(); tit != time_map.end(); tit++)
+   {
+      opencdev::cdev_time_t time = tit->first;
+      cout << time << "\t";
+      for(opencdev::result_t::const_iterator it = result.begin(); it != result.end(); it++)
+      {
+         const string &cell_name = it->first;
+         const map<opencdev::cdev_time_t, double> &values = it->second;
+         cout << values.at(time) << "\t";
+      }
+      cout << endl;
    }
 
    return EXIT_SUCCESS;
