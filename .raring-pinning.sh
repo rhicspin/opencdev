0412a8357743e8e3e92b687d61414a09404c8234
Dmitry Kalinkin <dkalinkin@bnl.gov>
2014-07-25 19:02:37 -0400

    travis: don't do raring pinning

--- .raring-pinning.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'APT::Default-Release "precise";' > /etc/apt/apt.conf.d/01ubuntu
-echo 'deb http://old-releases.ubuntu.com/ubuntu raring main' >> /etc/apt/sources.list
-echo 'Package: libboost*' >> /etc/apt/preferences
-echo 'Pin: release n=raring' >> /etc/apt/preferences
-echo 'Pin-Priority: 900' >> /etc/apt/preferences
